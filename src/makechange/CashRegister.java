0f210cc1cc220d4cbefd602046bea5bf08c2ba53
William Brown <wsbrown.colo@gmail.com>
2024-10-25 17:55:33 -0600

    user story 1 complete

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -1,10 +1,15 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class CashRegister {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner kb = new Scanner(System.in);
+		
+		//User Story #1
+		System.out.println("Excluding the dollar sign, please input the price of the purchase.");
+		double cost = kb.nextDouble();
 	}
 
 }
