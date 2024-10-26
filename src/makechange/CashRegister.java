5a23c1d1720156b9ac75f4623d9d9aa58423d0e2
William Brown <wsbrown.colo@gmail.com>
2024-10-25 18:00:35 -0600

    user story 2 complete

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -8,8 +8,17 @@ public class CashRegister {
 		Scanner kb = new Scanner(System.in);
 		
 		//User Story #1
-		System.out.println("Excluding the dollar sign, please input the price of the purchase.");
+		System.out.println("Excluding the dollar sign, please input the price of the purchase: ");
 		double cost = kb.nextDouble();
+		
+		//User Story #2
+		System.out.println("Please input the amount of money tendered: ");
+		double tender = kb.nextDouble();
+		
+		
+		
+		
+		kb.close();
 	}
 
 }
